ce472c04c409e2c3e65633c5f8ff1634dcc5f0c4
Volodymyr <kogeva19898@gmail.com>
2013-10-13 16:04:47 +0300

    add show message from notification

--- Reactr/src/main/java/com/example/reactr/fragments/ShowMessageFragment.java
+++ Reactr/src/main/java/com/example/reactr/fragments/ShowMessageFragment.java
@@ -13,7 +13,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
-import android.provider.MediaStore;
 import android.text.format.Time;
 import android.util.Log;
 import android.view.Display;
@@ -25,7 +24,6 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.ScaleAnimation;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -42,8 +40,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -51,7 +47,7 @@ import reactr.network.ReactorApi;
 import reactr.utils.ImageHelper;
 import reactr.utils.TakePhotoWithoutPreview;
 
-public class ShowMessageFragment extends SherlockFragment{
+public class ShowMessageFragment extends SherlockFragment {
 
     private MessageEntity message;
     private ImageView photoView;
@@ -66,11 +62,12 @@ public class ShowMessageFragment extends SherlockFragment{
     private Camera camera;
     private TakePhotoWithoutPreview ph;
     private TextView text;
-    private boolean reaction=false;
+    private boolean reaction = false;
     private View actionBarView;
 
     private Animation animationFadeIn, animationFadeOut;
     private Animation scaleAnimation, logoMoveAnimation;
+
     public ShowMessageFragment(MessageEntity message) {
         this.message = message;
     }
@@ -78,13 +75,13 @@ public class ShowMessageFragment extends SherlockFragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.show_message_layout, container, false);
-         text = (TextView) view.findViewById(R.id.message_text);
+        text = (TextView) view.findViewById(R.id.message_text);
 
-        photoView         = (ImageView) view.findViewById(R.id.photo_view);
+        photoView = (ImageView) view.findViewById(R.id.photo_view);
         reactionPhotoView = (ImageView) view.findViewById(R.id.reaction_photo_view);
-        surfaceView       = (SurfaceView) view.findViewById(R.id.hiddenPreview);
-        replyButton       = (ImageButton) view.findViewById(R.id.replyButton);
-        closeButton       = (ImageButton) view.findViewById(R.id.closeButton);
+        surfaceView = (SurfaceView) view.findViewById(R.id.hiddenPreview);
+        replyButton = (ImageButton) view.findViewById(R.id.replyButton);
+        closeButton = (ImageButton) view.findViewById(R.id.closeButton);
 
         replyButton.setOnClickListener(replyMessage);
         closeButton.setOnClickListener(closeClickListener);
@@ -97,7 +94,7 @@ public class ShowMessageFragment extends SherlockFragment{
 
         ReactrBase.showLoader(getSherlockActivity());
 
-        if((!message.getIsRead())&&(!message.getFromMe()))
+        if ((!message.getIsRead()) && (!message.getFromMe()))
             ph = new TakePhotoWithoutPreview(getSherlockActivity(), surfaceView);
         if (message.getText().length() == 0)
             text.setVisibility(View.INVISIBLE);
@@ -110,10 +107,10 @@ public class ShowMessageFragment extends SherlockFragment{
             @Override
             public void run() {
                 photo = downloadImage(message.getPhoto());
-                //****
+
                 if (!message.getReactionPhoto().equals("null"))
                     reactionPhoto = downloadImage(message.getReactionPhoto());
-                //******
+
                 handler.post(updateImageView);
             }
         }).start();
@@ -128,8 +125,7 @@ public class ShowMessageFragment extends SherlockFragment{
         return view;
     }
 
-    private Bitmap downloadImage (String url)
-    {
+    private Bitmap downloadImage(String url) {
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inSampleSize = 2;
 
@@ -149,10 +145,9 @@ public class ShowMessageFragment extends SherlockFragment{
             e.printStackTrace();
         }
 
-        for (int i = 1; i < 30 ; i++)
-        {
+        for (int i = 1; i < 30; i++) {
             try {
-                bmp = BitmapFactory.decodeStream( image , null, options);
+                bmp = BitmapFactory.decodeStream(image, null, options);
                 if (bmp != null)
                     return bmp;
             } catch (Exception e) {
@@ -170,39 +165,33 @@ public class ShowMessageFragment extends SherlockFragment{
         @Override
         public void run() {
             photoView.setImageBitmap(photo);
-            if (reactionPhoto != null)
-            {
+            if (reactionPhoto != null) {
                 //для округления изображения
-                Bitmap rounded_bm= ImageHelper.getRoundedCornerBitmap(reactionPhoto, Color.WHITE, getActivity().getApplicationContext());
+                Bitmap rounded_bm = ImageHelper.getRoundedCornerBitmap(reactionPhoto, Color.WHITE, getActivity().getApplicationContext());
                 reactionPhotoView.setImageBitmap(rounded_bm);
                 Log.d("SHOWMESSAGE", "ANIMATIONS");
                 Animation animation = AnimationUtils.loadAnimation(getActivity(), R.anim.scale);
                 reactionPhotoView.startAnimation(animation);
             }
-            //***********************************************
-            if((!message.getIsRead())&&(!message.getFromMe()))
-            {
+            if ((!message.getIsRead()) && (!message.getFromMe())) {
                 //прочтение сообщения
-                ReactorApi ra= ((MainActivity) getActivity()).getReactorApi();
+                ReactorApi ra = ((MainActivity) getActivity()).getReactorApi();
                 ra.readMessage(String.valueOf(message.getId()));
-                ((MainActivity)getActivity()).updateMenu();
+                ((MainActivity) getActivity()).updateMenu();
             }
             ReactrBase.hideLoader();
-            if((!message.getIsRead())&&(!message.getFromMe()))
-            {
+            if ((!message.getIsRead()) && (!message.getFromMe())) {
                 ph.takeReaction(message.getId());
             }
         }
     };
-    //***************************
 
     View.OnClickListener closeClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            ReactrBase.switchFraagment(getSherlockActivity(),new MailBoxFragment());
+            ReactrBase.switchFraagment(getSherlockActivity(), new MailBoxFragment());
         }
     };
-
 
     private View.OnClickListener switchPhoto = new View.OnClickListener() {
         @Override
@@ -218,8 +207,7 @@ public class ShowMessageFragment extends SherlockFragment{
         }
     };
 
-    public static Bitmap RotateBitmap(Bitmap source, float angle)
-    {
+    public static Bitmap RotateBitmap(Bitmap source, float angle) {
         Matrix matrix = new Matrix();
         matrix.postRotate(angle);
         Bitmap newBitmap = Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
@@ -234,24 +222,24 @@ public class ShowMessageFragment extends SherlockFragment{
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setTitle("Select a photo to save");
 
-            CharSequence[]cs;
-            if(reactionPhoto!=null)
-                cs= new CharSequence[]{getResources().getString(R.string.sv_ph_mes), getResources().getString(R.string.sv_ph_react),
+            CharSequence[] cs;
+            if (reactionPhoto != null)
+                cs = new CharSequence[]{getResources().getString(R.string.sv_ph_mes), getResources().getString(R.string.sv_ph_react),
                         getResources().getString(R.string.cancel)};
             else
-                cs= new CharSequence[]{getResources().getString(R.string.sv_ph_mes), getResources().getString(R.string.cancel)};
+                cs = new CharSequence[]{getResources().getString(R.string.sv_ph_mes), getResources().getString(R.string.cancel)};
             builder.setItems(cs, new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int which) {
                     Time now = new Time();
                     now.setToNow();
-                    String str="IMG_"+now.year+"_"+now.month+"."+now.monthDay+"_"+now.hour+":"+now.minute+":"+now.second;
-                    if(which==0){
+                    String str = "IMG_" + now.year + "_" + now.month + "." + now.monthDay + "_" + now.hour + ":" + now.minute + ":" + now.second;
+                    if (which == 0) {
                         savePhotoToMyAlbum(str, photo);
                     }
-                    if(which==1&&reactionPhoto!=null){
+                    if (which == 1 && reactionPhoto != null) {
                         savePhotoToMyAlbum(str, reactionPhoto);
                     }
-                    if(which==2||(which==1&&reactionPhoto==null)){
+                    if (which == 2 || (which == 1 && reactionPhoto == null)) {
                         dialog.cancel();
                     }
                 }
@@ -266,6 +254,7 @@ public class ShowMessageFragment extends SherlockFragment{
             dialog.show();
         }
     };
+
     //переключение фото с "маленького на большое"
     private View.OnClickListener switchPhotos = new View.OnClickListener() {
         @Override
@@ -275,18 +264,16 @@ public class ShowMessageFragment extends SherlockFragment{
             photoView.startAnimation(animationFadeOut);
             reactionPhotoView.startAnimation(animationFadeOut);
 
-            if(!reaction)
-            {
+            if (!reaction) {
                 photoView.setImageBitmap(reactionPhoto);
-                Bitmap rounded_bm= ImageHelper.getRoundedCornerBitmap(photo, Color.WHITE, getActivity().getApplicationContext());
+                Bitmap rounded_bm = ImageHelper.getRoundedCornerBitmap(photo, Color.WHITE, getActivity().getApplicationContext());
                 reactionPhotoView.setImageBitmap(rounded_bm);
-            }
-            else{
+            } else {
                 photoView.setImageBitmap(photo);
-                Bitmap rounded_bm= ImageHelper.getRoundedCornerBitmap(reactionPhoto, Color.WHITE, getActivity().getApplicationContext());
+                Bitmap rounded_bm = ImageHelper.getRoundedCornerBitmap(reactionPhoto, Color.WHITE, getActivity().getApplicationContext());
                 reactionPhotoView.setImageBitmap(rounded_bm);
             }
-            reaction=!reaction;
+            reaction = !reaction;
             photoView.startAnimation(animationFadeIn);
             reactionPhotoView.startAnimation(animationFadeIn);
 
@@ -294,16 +281,15 @@ public class ShowMessageFragment extends SherlockFragment{
     };
 
 
-    private void savePhotoToMyAlbum (String filename, Bitmap bitmap)
-    {
-        String fullPath=Environment
-                .getExternalStorageDirectory() + File.separator+Environment.DIRECTORY_DCIM
+    private void savePhotoToMyAlbum(String filename, Bitmap bitmap) {
+        String fullPath = Environment
+                .getExternalStorageDirectory() + File.separator + Environment.DIRECTORY_DCIM
                 + "/Reactor/";
         File dir = new File(fullPath);
         if (!dir.exists()) {
             dir.mkdirs();
         }
-        File image = new File(fullPath, filename+".jpg");
+        File image = new File(fullPath, filename + ".jpg");
         boolean success = false;
         // Encode the file as a PNG image.
         FileOutputStream outStream;
@@ -329,10 +315,26 @@ public class ShowMessageFragment extends SherlockFragment{
         //*******************************************
         Intent mediaScanIntent = new Intent(
                 Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        String currentPath = fullPath+File.separator+filename+".jpg";
+        String currentPath = fullPath + File.separator + filename + ".jpg";
         File f = new File(currentPath);
         Uri contentUri = Uri.fromFile(f);
         mediaScanIntent.setData(contentUri);
         getActivity().sendBroadcast(mediaScanIntent);
-        }
     }
+
+    private boolean confirmReaction()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getSherlockActivity());
+        builder.setMessage("").setTitle("");
+
+        builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        return true;
+    }
+}
